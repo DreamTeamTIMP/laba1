1b177f7c39ed0c27a6dbe87f6fbfa37769237614
egor0928931 <egor0928931@gmail.com>
2026-02-11 19:25:38 +0700

    console things

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq.Expressions;
+
+
+namespace laba1;
+
+public class Program
+{
+    static public int Main()
+    {
+        while(true)
+        {
+            try
+            {
+                Console.Write("PS> ");
+                string? input = Console.ReadLine();
+
+                if(string.IsNullOrEmpty(input))
+                    throw new Exception("Incorrect input!");
+            
+                    
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Error: {exception.Message}");
+            }
+
+        }
+
+
+
+        return 0;
+    }
+}
